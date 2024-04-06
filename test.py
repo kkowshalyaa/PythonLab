54ee64e15bc0e243dd5d07385639c2d85d8e011e
pooja-guru25 <76553801+pooja-guru25@users.noreply.github.com>
2024-04-06 22:09:39 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 
 import csv
+import pandas
+count = 0 
 with open('sample.csv', mode ='r')as file:
   csvFile = csv.reader(file)
   for lines in csvFile:
+        count += 1
         print(lines)
+        #Break after printing 10 lines
+        if count == 10:
+          break
